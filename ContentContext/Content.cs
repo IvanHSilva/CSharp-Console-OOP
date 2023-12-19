ad8981f7366333abc0e49ad52a88f8172f37c48e
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-12-19 16:31:21 -0300

    Content class improvements

--- ContentContext/Content.cs
+++ ContentContext/Content.cs
@@ -1,9 +1,14 @@
 namespace Oop.ContentContext
 {
-    public class Content
+    public abstract class Content
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
+
+        public Content()
+        {
+            Id = Guid.NewGuid(); //SPOF - Single Point Of Fail
+        }
     }
 }
