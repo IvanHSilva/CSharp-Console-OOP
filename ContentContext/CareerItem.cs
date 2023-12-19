cfdeabed36ad73d7bb924a9292b7ccfa9a6d6839
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-12-19 18:49:02 -0300

    List of articles, courses and careers

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -1,10 +1,10 @@
 namespace Oop.ContentContext
 {
-    public class CareerItem
+    public class CareerItem(int order, string title, string description, Course course)
     {
-        public int Order { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public Course Course { get; set; } = null!;
+        public int Order { get; set; } = order;
+        public string Title { get; set; } = title;
+        public string Description { get; set; } = description;
+        public Course Course { get; set; } = course;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Oop.ContentContext;
+﻿using Oop.ContentContext;
 
 namespace Oop
 {
@@ -7,18 +6,48 @@ namespace Oop
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.WriteLine();
+
             var articles = new List<Article>()!;
             articles.Add(new Article("Orientação a Objetos", "orientacao-objetos"));
             articles.Add(new Article("Linguagem C#", "linguagem-csharp"));
             articles.Add(new Article("Plataforma .NET", "plataforma-net"));
-            Console.Clear();
-            Console.WriteLine();
-            Console.WriteLine("Artigos:");
+            Console.WriteLine("Artigos: (Id / Titulo / URL)");
             foreach (var article in articles)
             {
-                Console.WriteLine(article.Id);
-                Console.WriteLine(article.Title);
-                Console.WriteLine(article.Url);
+                Console.WriteLine($"{article.Id} / {article.Title} / {article.Url}");
+            }
+            Console.WriteLine();
+
+            var courses = new List<Course>()!;
+            courses.Add(new Course("Fundamentos OOP", "fundamentos-oop"));
+            courses.Add(new Course("Fundamentos C#", "fundamentos-csharp"));
+            courses.Add(new Course("Fundamentos .NET", "fundamentos-net"));
+            Console.WriteLine("Cursos: (Id / Titulo / URL)");
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Id} / {course.Title} / {course.Url}");
+            }
+            Console.WriteLine();
+
+            var careers = new List<Career>()!;
+            var career = new Career("Fundamentos C#", "fundamentos-csharp");
+            var careerItem = new CareerItem(1, "Início do Curso", "", null!);
+            career.Courses.Add(careerItem);
+            careerItem = new CareerItem(2, "Fundamentos Básicos", "", null!);
+            career.Courses.Add(careerItem);
+            careerItem = new CareerItem(3, "Introdução ao C#", "", null!);
+            career.Courses.Add(careerItem);
+            careers.Add(career);
+            Console.WriteLine("Carreira: (Titulo / Cursos)");
+            foreach (var car in careers)
+            {
+                Console.WriteLine($"{car.Title}");
+                foreach (var item in career.Courses.OrderBy(c => c.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
             }
             Console.WriteLine();
         }
